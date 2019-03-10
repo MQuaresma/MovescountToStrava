42a59fec5e90c501a58e2213cf3d417b6aafb54b
MQuaresma <miguelquaresma97@gmail.com>
2019-03-10 21:24:30 +0000

    Moves are now correctly exported and debuggin session is now supported

--- export.py
+++ export.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
 
 import requests
+import sys, getopt
 from getpass import getpass
 
 movescount_domain = "http://www.movescount.com"
 
-def getSession():
+def generateSessionCookie():
+    moves_uname = input("Movescount email: ")
+    moves_pwd = getpass("Movescount password: ")
+    #TODO: RE cookie generation mechanism
+    
+def generateDebugSessionCookie():
     cookie_dir = "cookies/"
     cookies = {'MovesCountCookie':'', 'ASP.NET_SessionId':'', 'AWSELB':''}
     cookies['MovesCountCookie'] = open(cookie_dir+"movescount_movecountcookie").read()
     cookies['ASP.NET_SessionId'] = open(cookie_dir+"movescount_sessionid").read()
     cookies['AWSELB'] = open(cookie_dir+"movescount_awselb").read()
+    return cookies
+
+def getSession(cookies):
     session = requests.get(movescount_domain + '/latestmove', cookies=cookies)
 
     if(session.status_code == 200):
         print("Authencation successful \nFetching moves...")
-        return cookies
+        return True
     else:
         print("Authencation Failed")
-        return None
+        return False
 
 def getMoveIDs():
     # TODO: crawl for Moves
@@ -28,13 +37,21 @@ def getMoveIDs():
 def exportMove(cookies, move_id, exp_format = "gpx"):
     payload = {'id': move_id, 'format': exp_format}
     exp_move = requests.get(movescount_domain + "/move/export" , params=payload, cookies=cookies)
-    print(exp_move.status_code)
+    if(exp_move.status_code == 200):
+        move_dump = open(move_id + "." + exp_format, "wb")
+        move_dump.write(exp_move.content)
+        move_dump.close()
 
 def main():
-    #moves_uname = input("Movescount email: ")
-    #moves_pwd = getpass("Movescount password: ")
-    session_cookie = getSession()
-    if session_cookie:
+    args, _ = getopt.getopt(sys.argv[1:], "d")
+    args = dict(args) 
+    if "-d" in args:
+        print("Creating debug session..")
+        session_cookie = generateDebugSessionCookie()
+    else:
+        session_cookie = generateSessionCookie()
+    
+    if getSession(session_cookie):
         moves = getMoveIDs()
         if len(moves) > 0:
             for move_id in moves:
@@ -42,6 +59,8 @@ def main():
             print("All moves exported")
         else:
             print("No moves to export")
+    else:
+        print("Exiting...")
     
 
 if __name__ == "__main__":
